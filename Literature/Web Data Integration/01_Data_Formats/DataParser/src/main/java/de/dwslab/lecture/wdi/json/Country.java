514e1ecea92d12808824cfec039181f1538d9447
Dominik Boehler <dominik.boehler@gmx.net>
2024-03-01 15:58:24 +0100

    updates

--- Literature/Web Data Integration/01_Data_Formats/DataParser/src/main/java/de/dwslab/lecture/wdi/json/Country.java
+++
@@ -1,8 +0,0 @@
-package de.dwslab.lecture.wdi.json;
-
-public class Country {
-	String id;
-	String name;
-	String car_code;
-	Long population;
-}
--- Literature/Web Data Integration/01_Data_Formats/DataParser/src/main/java/de/dwslab/lecture/wdi/rdf/Country.java
+++
@@ -1,9 +0,0 @@
-package de.dwslab.lecture.wdi.rdf;
-
-public class Country {
-	String id;
-	String name;
-	Long population;
-	String[] languages;
-
-}
